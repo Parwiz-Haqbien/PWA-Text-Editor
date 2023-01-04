dd7498300709fe5464e49cb5d69be76af94fef01
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-01-04 22:49:31 +1100

    added asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,12 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 
-registerRoute()
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }));
