227ecbc7e82ad982a3dc90b2179fb367447e3741
Parwiz-Haqbien <Prwiz.jan03@gmail.com>
2023-01-04 23:15:06 +1100

    added logic to accept content/add it to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,10 +13,31 @@ const initDb = async () =>
   });
 
 
-export const putDb = async (content) => console.log('Put to the database');
+export const putDb = async (content) => {
+  console.log('Put to the database');
 
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  console.log(request);
 
-export const getDb = async () => console.log('getDb not implemented');
+  const result = await request;
+  console.log('data saved to the database', result);
+  return result;
+}
 
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+}
 
 initDb();
